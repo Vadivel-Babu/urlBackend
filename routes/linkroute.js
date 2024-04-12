76e06ff4a91af4131d6549d359532914cd0134d5
vadivel <vadivelbabu1996@gmail.com>
2024-04-12 16:00:00 +0530

    fix error

--- routes/linkroute.js
+++ routes/linkroute.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import linkController from "../controllers/linkController.js";
-import auth from "../middleware/authmiddleware.js";
+//import auth from "../middleware/authmiddleware.js";
 
 const route = Router();
 
 route.get("/", (req, res) =>
   res.json({ message: "API working", url: req.baseUrl + "/getlinks" })
 );
-route.get("/getlinks", auth, linkController.getLinks);
-route.post("/postlink", auth, linkController.postLink);
-route.delete("/deletlink/:id", auth, linkController.deleteLink);
+route.get("/getlinks", linkController.getLinks);
+route.post("/postlink", linkController.postLink);
+route.delete("/deletlink/:id", linkController.deleteLink);
 
 export default route;
